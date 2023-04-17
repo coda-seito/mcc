71c9a8b54e6e62deb94dbf145d42ece0821bf9a7
fkhan <fkhan@edmunds.com>
2023-04-16 18:29:52 -0700

    updated

--- src/markup/Pages/resources/data.js
+++ src/markup/Pages/resources/data.js
@@ -5,37 +5,37 @@ const items = {
             "name": "Vanilla",
             "half-kilo-price": 250,
             "kilo-price": 399,
-            "pic": "product/images/cake/vanila.jpeg"
+            "pic": "product/images/cake/vanila.jpg"
         },
         {
             "name": "Black Forest",
             "half-kilo-price": 250,
             "kilo-price": 399,
-            "pic": "product/images/cake/black-forest.jpeg"
+            "pic": "product/images/cake/black-forest.jpg"
         },
         {
             "name": "Green Apple",
             "half-kilo-price": 250,
             "kilo-price": 399,
-            "pic": "product/images/cake/green-apple.jpeg"
+            "pic": "product/images/cake/green-apple.jpg"
         },
         {
             "name": "Strawberry",
             "half-kilo-price": 250,
             "kilo-price": 499,
-            "pic": "product/images/cake/strawberry.jpeg"
+            "pic": "product/images/cake/strawberry.jpg"
         },
         {
             "name": "Mango",
             "half-kilo-price": 250,
             "kilo-price": 499,
-            "pic": "product/images/cake/mango.jpeg"
+            "pic": "product/images/cake/mango.jpg"
         },
         {
             "name": "Pineapple",
             "half-kilo-price": 250,
             "kilo-price": 499,
-            "pic": "product/images/cake/pineapple.jpeg"
+            "pic": "product/images/cake/pineapple.jpg"
         },
         {
             "name": "Blueberry",
@@ -47,43 +47,91 @@ const items = {
             "name": "Butterscotch",
             "half-kilo-price": 250,
             "kilo-price": 599,
-            "pic": "product/images/cake/butterscotch.jpeg"
+            "pic": "product/images/cake/butterscotch.jpg"
         },
         {
             "name": "BlackCurrent",
             "half-kilo-price": 250,
-            "kilo-price": 599,
-            "pic": "product/images/cake/blackcurrent.jpeg"
+            "kilo-price": 499,
+            "pic": "product/images/cake/blackcurrent.jpg"
         },
         {
             "name": "Orange",
             "half-kilo-price": 250,
             "kilo-price": 599,
-            "pic": "product/images/cake/orange.jpeg"
+            "pic": "product/images/cake/orange.jpg"
         },
         {
             "name": "Choco Almond",
-            "half-kilo-price": 250,
-            "kilo-price": 599,
-            "pic": "product/images/cake/chocoalmond.jpeg"
+            "half-kilo-price": 399,
+            "kilo-price": 750,
+            "pic": "product/images/cake/chocoalmond.jpg"
         },
         {
             "name": "Chocolate Truffle",
-            "half-kilo-price": 250,
-            "kilo-price": 599,
-            "pic": "product/images/cake/chocolatetruffle.jpeg"
+            "half-kilo-price": 350,
+            "kilo-price": 699,
+            "pic": "product/images/cake/chocolatetruffle.jpg"
         },
         {
             "name": "Honey Almond",
+            "half-kilo-price": 399,
+            "kilo-price": 750,
+            "pic": "product/images/cake/almondhoney.jpg"
+        },
+        {
+            "name": "Irish Coffee",
             "half-kilo-price": 250,
-            "kilo-price": 599,
-            "pic": "product/images/cake/almondhoney.jpeg"
+            "kilo-price": 499,
+            "pic": "product/images/cake/irishcoffee.jpg"
         },
         {
             "name": "Red Velvet",
-            "half-kilo-price": 250,
-            "kilo-price": 599,
-            "pic": "product/images/cake/redvelvet.jpeg"
+            "half-kilo-price": 450,
+            "kilo-price": 900,
+            "pic": "product/images/cake/redvelvet.jpg"
+        },
+        {
+            "name": "Rasa Malai",
+            "half-kilo-price": 450,
+            "kilo-price": 900,
+            "pic": "product/images/cake/rasamalai.jpg"
+        },
+        {
+            "name": "White Forest",
+            "half-kilo-price": 300,
+            "kilo-price": 500,
+            "pic": "product/images/cake/whiteforest.jpg"
+        },
+        {
+            "name": "Choco Blast",
+            "half-kilo-price": 350,
+            "kilo-price": 699,
+            "pic": "product/images/cake/chocolateblast.jpg"
+        },
+        {
+            "name": "Rainbow Cake",
+            "half-kilo-price": 400,
+            "kilo-price": 700,
+            "pic": "product/images/cake/rainbow.jpg"
+        },
+        {
+            "name": "Choco Fudge",
+            "half-kilo-price": 400,
+            "kilo-price": 799,
+            "pic": "product/images/cake/chocofudge.jpg"
+        },
+        {
+            "name": "Choco Fantasy",
+            "half-kilo-price": 400,
+            "kilo-price": 899,
+            "pic": "product/images/cake/chocofantasy.jpg"
+        },
+        {
+            "name": "Fresh Fruit Cake",
+            "half-kilo-price": 400,
+            "kilo-price": 799,
+            "pic": "product/images/cake/fruitcake.png"
         }
     ]
 }
